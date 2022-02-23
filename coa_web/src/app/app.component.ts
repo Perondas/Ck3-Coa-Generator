395bcb9794b3249a3a59ef587b90782aa3a06be3
Perondas <Pperondas@gmail.com>
2022-02-23 18:47:55 +0100

    Corrected dependencies and applied formatting

--- coa_web/src/app/app.component.ts
+++ coa_web/src/app/app.component.ts
@@ -1,97 +1,109 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { FileValidators } from 'ngx-file-drag-drop';
-import { coa_from_image_vanilla_colors, coa_from_image_all_colors, coa_from_image_reduced_colors } from 'coa_converter_wasm';
+import {
+  coa_from_image_vanilla_colors,
+  coa_from_image_all_colors,
+  coa_from_image_reduced_colors,
+} from 'coa_converter_wasm';
 import { MatDialog } from '@angular/material/dialog';
 import { MessageBoxComponent } from './Dialogs/message-box/message-box.component';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(
-    private dialog: MatDialog
-  ) {}
+  constructor(private dialog: MatDialog) {}
 
   title = 'coa';
-  fileControl = new FormControl([], [FileValidators.required, FileValidators.uniqueFileNames]);
-  resolution=100;
+  fileControl = new FormControl(
+    [],
+    [FileValidators.required, FileValidators.uniqueFileNames]
+  );
+  resolution = 100;
   isTitle = false;
   showControls = false;
-  result = "";
+  result = '';
 
-
-  delay(n: number){
-    return new Promise(function(resolve){
-        setTimeout(resolve,n);
+  delay(n: number) {
+    return new Promise(function (resolve) {
+      setTimeout(resolve, n);
     });
-}
+  }
 
   async onReducedColors() {
-    this.result = "";
+    this.result = '';
     await this.delay(50);
     let file = this.fileControl.value[0] as File;
     let buffer = await file.arrayBuffer();
     let array = new Uint8Array(buffer);
-    coa_from_image_reduced_colors(array, this.isTitle, 20, this.resolution).then(result => {
-      if(result){
-        this.result = result;
-      }
-      else {
+    coa_from_image_reduced_colors(array, this.isTitle, 20, this.resolution)
+      .then((result) => {
+        if (result) {
+          this.result = result;
+        } else {
+          this.dialog.open(MessageBoxComponent, {
+            data: {
+              message: 'A error occurred. Make sure to submit a valid image!',
+            },
+          });
+        }
+      })
+      .catch(() => {
         this.dialog.open(MessageBoxComponent, {
-          data: { message: 'A error occurred. Make sure to submit a valid image!' },
+          data: { message: 'Something went wrong! Try reloading the page!' },
         });
-      }
-    }).catch(() => {
-      this.dialog.open(MessageBoxComponent, {
-        data: { message: 'Something went wrong! Try reloading the page!' },
       });
-    });   
   }
 
   async onAllColors() {
-    this.result = "";
+    this.result = '';
     let file = this.fileControl.value[0] as File;
     let buffer = await file.arrayBuffer();
     let array = new Uint8Array(buffer);
-    coa_from_image_all_colors(array, this.isTitle, this.resolution).then(result => {
-      if(result){
-        this.result = result;
-      }
-      else {
+    coa_from_image_all_colors(array, this.isTitle, this.resolution)
+      .then((result) => {
+        if (result) {
+          this.result = result;
+        } else {
+          this.dialog.open(MessageBoxComponent, {
+            data: {
+              message: 'A error occurred. Make sure to submit a valid image!',
+            },
+          });
+        }
+      })
+      .catch(() => {
         this.dialog.open(MessageBoxComponent, {
-          data: { message: 'A error occurred. Make sure to submit a valid image!' },
+          data: { message: 'Something went wrong! Try reloading the page!' },
         });
-      }
-    }).catch(() => {
-      this.dialog.open(MessageBoxComponent, {
-        data: { message: 'Something went wrong! Try reloading the page!' },
       });
-    });   
   }
 
   async onVanillaColors() {
-    this.result = "";
+    this.result = '';
     let file = this.fileControl.value[0] as File;
     let buffer = await file.arrayBuffer();
     let array = new Uint8Array(buffer);
-    coa_from_image_vanilla_colors(array, this.isTitle, this.resolution).then(result => {
-      if(result){
-        this.result = result;
-      }
-      else {
+    coa_from_image_vanilla_colors(array, this.isTitle, this.resolution)
+      .then((result) => {
+        if (result) {
+          this.result = result;
+        } else {
+          this.dialog.open(MessageBoxComponent, {
+            data: {
+              message: 'A error occurred. Make sure to submit a valid image!',
+            },
+          });
+        }
+      })
+      .catch(() => {
         this.dialog.open(MessageBoxComponent, {
-          data: { message: 'A error occurred. Make sure to submit a valid image!' },
+          data: { message: 'Something went wrong! Try reloading the page!' },
         });
-      }
-    }).catch(() => {
-      this.dialog.open(MessageBoxComponent, {
-        data: { message: 'Something went wrong! Try reloading the page!' },
       });
-    });   
   }
 
   onValueChange() {
@@ -99,6 +111,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      this.fileControl.valueChanges.subscribe(() => this.onValueChange());
+    this.fileControl.valueChanges.subscribe(() => this.onValueChange());
   }
 }
--- coa_web/src/app/app.module.ts
+++ coa_web/src/app/app.module.ts
@@ -5,22 +5,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSliderModule } from '@angular/material/slider';
 import { FormsModule } from '@angular/forms';
-import {ClipboardModule} from '@angular/cdk/clipboard';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MessageBoxComponent } from './Dialogs/message-box/message-box.component';
-import {MatTooltipModule, TooltipComponent} from '@angular/material/tooltip';
+import { MatTooltipModule, TooltipComponent } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MessageBoxComponent
-  ],
+  declarations: [AppComponent, MessageBoxComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -36,9 +33,9 @@ import {MatTooltipModule, TooltipComponent} from '@angular/material/tooltip';
     ClipboardModule,
     MatCheckboxModule,
     MatDialogModule,
-    MatTooltipModule
+    MatTooltipModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
